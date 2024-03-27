d8c970b6aa47d94a6c1aecb21de96597118326e2
Madhu Rajagopal <rajagopal.madhu@gmail.com>
2024-03-27 03:05:28 +0000

    Chore: Remove commented out code

--- install-cafe.sh
+++ install-cafe.sh
@@ -2,7 +2,6 @@
 set -u
 
 for ((i = 0; i < "${1}"; i++)) {
-#	helm install nginx-ingress-${i} oci://ghcr.io/nginxinc/charts/nginx-ingress --version 0.18.0 -f ./values.yaml --namespace nginx-ingress-${i} --create-namespace --set controller.ingressClass="nginx-ingress-${i}" --set controller.image.tag="3.2.0"
     helm install nic-${i} cafe-chart --set ingressClass.name="nginx-ingress-${i}" --set number=${i}
 }
 
