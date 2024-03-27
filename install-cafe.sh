5f9e2a9ca62baea90e254b9667596bc594beff35
Madhu Rajagopal <rajagopal.madhu@gmail.com>
2024-03-27 04:26:13 +0000

    Fix: Improve cafe scripts to customise helm name

--- install-cafe.sh
+++ install-cafe.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -u
+COUNT=${1}
+NAME=${2}
 
-for ((i = 0; i < "${1}"; i++)) {
-    helm install nic-${i} cafe-chart --set ingressClass.name="nginx-ingress-${i}" --set number=${i}
+for ((i = 0; i < "${COUNT}"; i++)) {
+    INGRESS_CLASS_NAME="${NAME}-${i}"
+    HELM_INSTALL_NAME="app-${INGRESS_CLASS_NAME}"
+    helm install ${HELM_INSTALL_NAME} cafe-chart --set ingressClass.name="${INGRESS_CLASS_NAME}" --set number=${i}
 }
 
--- uninstall-cafe.sh
+++ uninstall-cafe.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -u
+COUNT=${1}
+NAME=${2}
 
-for ((i = 0; i < "${1}"; i++)) {
-    helm uninstall nic-${i} #cafe-chart --set ingressClass.name="nginx-ingress-${i}" --set number=${i}
+for ((i = 0; i < "${COUNT}"; i++)) {
+    HELM_INSTALL_NAME="${NAME}-${i}"
+    helm uninstall ${HELM_INSTALL_NAME} 
 }
 
