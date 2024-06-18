6406441a0b2d3184e8203f96e111a4073f8eeb6e
Madhu Rajagopal <rajagopal.madhu@gmail.com>
2024-06-18 02:49:33 +0000

    chore: Use variables for chart and NIC versions

--- nginx-ingress-app/install.sh
+++ nginx-ingress-app/install.sh
@@ -2,6 +2,8 @@
 set -u
 COUNT=${1}
 NAME=${2}
+CHART_VERSION=1.2.1
+NIC_VERSION="3.5.1"
 read -sp 'Please enter JWT Token if using a private container registry: ' JWT_TOKEN
 
 for ((i = 0; i < ${COUNT}; i++)) {
@@ -10,5 +12,5 @@ for ((i = 0; i < ${COUNT}; i++)) {
 	INGRESS_CLASS_NAME=${NAMESPACE}
 	kubectl create ns ${NAMESPACE}
 	kubectl create secret docker-registry regcred --docker-server=private-registry.nginx.com --docker-username=$JWT_TOKEN --docker-password=none -n ${NAMESPACE}
-	helm install ${HELM_INSTALL_NAME} oci://ghcr.io/nginxinc/charts/nginx-ingress --version 0.18.0 -f ./values.yaml --namespace ${NAMESPACE} --create-namespace --set controller.ingressClass="${INGRESS_CLASS_NAME}" --set controller.image.tag="3.2.0"
+	helm install ${HELM_INSTALL_NAME} oci://ghcr.io/nginxinc/charts/nginx-ingress --version ${CHART_VERSION}  -f ./values.yaml --namespace ${NAMESPACE} --create-namespace --set controller.ingressClass.name="${INGRESS_CLASS_NAME}" --set controller.image.tag=${NIC_VERSION}
 }
