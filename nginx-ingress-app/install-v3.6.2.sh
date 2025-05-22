3bd8036fd31fcd5c2e71f37e9efffe467277d11e
Madhu Rajagopal <rajagopal.madhu@gmail.com>
2025-05-22 22:17:54 +0000

    feat: tailor the install to specific versions of NIC

---
+++ nginx-ingress-app/install-v3.6.2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -u
+COUNT=${1}
+NAME=${2}
+CHART_VERSION=1.3.2
+NIC_VERSION="3.6.2"
+VALUES_FILE="./values-wafv5.yaml"
+read -sp 'Please enter JWT Token if using a private container registry: ' JWT_TOKEN
+
+for ((i = 0; i < ${COUNT}; i++)) {
+	HELM_INSTALL_NAME=${NAME}-${i}
+	NAMESPACE=${HELM_INSTALL_NAME}
+	INGRESS_CLASS_NAME=${NAMESPACE}
+	kubectl create ns ${NAMESPACE}
+	kubectl create secret docker-registry regcred --docker-server=private-registry.nginx.com --docker-username=$JWT_TOKEN --docker-password=none -n ${NAMESPACE}
+	helm install ${HELM_INSTALL_NAME} oci://ghcr.io/nginxinc/charts/nginx-ingress --version ${CHART_VERSION}  -f ${VALUES_FILE} --namespace ${NAMESPACE} --create-namespace --set controller.ingressClass.name="${INGRESS_CLASS_NAME}" --set controller.image.tag=${NIC_VERSION}
+        helm install nic-podmon-${NAMESPACE} nic-podmon --namespace ${NAMESPACE}
+}
---
+++ nginx-ingress-app/install-v4.0.1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -u
+COUNT=${1}
+NAME=${2}
+CHART_VERSION=2.0.1
+NIC_VERSION="4.0.1"
+TAG="4.0.1-ubi"
+read -sp 'Please enter JWT Token if using a private container registry: ' JWT_TOKEN
+read -sp 'Please enter license JWT: ' JWT_LICENSE
+for ((i = 0; i < ${COUNT}; i++)) {
+	HELM_INSTALL_NAME=${NAME}-${i}
+	NAMESPACE=${HELM_INSTALL_NAME}
+	INGRESS_CLASS_NAME=${NAMESPACE}
+	kubectl create ns ${NAMESPACE}
+	kubectl create secret docker-registry regcred --docker-server=private-registry.nginx.com --docker-username=$JWT_TOKEN --docker-password=none -n ${NAMESPACE}
+	kubectl create secret generic license-token --from-literal=license.jwt=$JWT_LICENSE --type=nginx.com/license -n ${NAMESPACE}
+#	helm install ${HELM_INSTALL_NAME} oci://ghcr.io/nginxinc/charts/nginx-ingress --version ${CHART_VERSION}  -f ./values.yaml --namespace ${NAMESPACE} --create-namespace --set controller.ingressClass.name="${INGRESS_CLASS_NAME}" --set controller.image.tag=${NIC_VERSION}
+	helm install ${HELM_INSTALL_NAME} oci://ghcr.io/nginx/charts/nginx-ingress --version ${CHART_VERSION}  -f ./values.yaml --namespace ${NAMESPACE} --create-namespace --set controller.ingressClass.name="${INGRESS_CLASS_NAME}" --set controller.image.tag=${TAG}
+         helm install nic-podmon-${NAMESPACE} nic-podmon --namespace ${NAMESPACE}
+}
